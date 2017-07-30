77bbcc54adba0e9e67b4fbdae2f584f723fda39f
isaigarciamoso <ing.isaimosso@gmail.com>
2017-07-30 18:21:51 -0500

    Initial commit

---
+++ app/src/main/java/com/isaigarciamoso/coornedas/MainActivity.java
@@ -0,0 +1,68 @@
+package com.isaigarciamoso.coornedas;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    private LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 1, this);
+
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        System.out.println("estoy en onLocationChanged: "+location);
+
+        String msg = "New Latitude: " + location.getLatitude()
+                + "New Longitude: " + location.getLongitude();
+
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        System.out.println("ESTOY EN onStatusChanged "+provider+" "+status+" "+extras);
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        System.out.println("PROVIER: "+provider);
+    }
+    @Override
+    public void onProviderDisabled(String provider) {
+         System.out.println("PROVIDER: "+provider);
+
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+
+
+}
